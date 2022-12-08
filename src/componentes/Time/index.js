64f73e4c28f0abcfeec6b3211d3c357ad328baad
Erik Henrique Rodrigues <113520508+erikhrodrigues@users.noreply.github.com>
2022-12-08 05:34:23 -0300

    Upload

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,24 @@
+import Colaborador from '../../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+    const css = {backgroundColor: props.corPrimaria}
+
+    return (
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corSecundaria}}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => 
+                    <Colaborador nome={colaborador.nome}
+                    corDeFundo={props.corSecundaria} 
+                    key={colaborador.nome}
+                    cargo={colaborador.cargo} 
+                    imagem={colaborador.imagem} 
+                />)}
+            </div>
+        </section>
+        : ""
+    )
+}
+
+export default Time
